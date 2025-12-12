a2abd57c9424a5462cf79065849a99b09d9134f7
Pazirs <parisdeep.singh@edu.esiee.fr>
2025-12-13 00:15:46 +0200

    update

--- main.go
+++ main.go
@@ -14,12 +14,33 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Setup des routes API
-	api.SetupRoutes()
+	// Création du mux
+	mux := http.NewServeMux()
 
-	// Redirection des short codes
-	http.HandleFunc("/", api.RedirectHandler) // Gestion de toutes les redirections
+	// Setup des routes API sur ce mux
+	api.SetupRoutes(mux) // modification nécessaire dans routes.go pour accepter un mux
+
+	// Redirection des short codes (toutes les autres routes)
+	mux.HandleFunc("/", api.RedirectHandler)
+
+	// Middleware CORS complet
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Autoriser le front-end
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Répondre aux requêtes OPTIONS (pré-vol)
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Passer la requête au mux
+		mux.ServeHTTP(w, r)
+	})
 
 	log.Println("Server is running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
