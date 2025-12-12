46ad0a9789d0d500081e78ab109a58b8175d3106
Pazirs <parisdeep.singh@edu.esiee.fr>
2025-12-13 01:14:00 +0200

    update

--- url-shortener-frontend/src/Home.jsx
+++ url-shortener-frontend/src/Home.jsx
@@ -1,27 +1,14 @@
-// src/Home.jsx
-
 import { Link } from "react-router-dom";
+import "./Home.css"; // nouveau fichier CSS
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", marginTop: "80px" }}>
+    <div className="home-container">
       <h1>Raccourcissez vos liens. Analysez vos clics.</h1>
-
-      <p style={{ fontSize: 18, marginTop: 10 }}>
+      <p className="home-subtitle">
         Un raccourcisseur d’URL intelligent avec statistiques détaillées.
       </p>
-
-      <Link
-        to="/login"
-        style={{
-          marginTop: 30,
-          display: "inline-block",
-          padding: "12px 25px",
-          background: "#007bff",
-          color: "#fff",
-          borderRadius: 8,
-        }}
-      >
+      <Link to="/login" className="home-button">
         Commencer maintenant
       </Link>
     </div>
