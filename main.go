79a5e810e4e231d714ab25494b87b49afc4d4acd
Pazirs <parisdeep.singh@edu.esiee.fr>
2026-01-31 23:27:44 +0100

    update

--- main.go
+++ main.go
@@ -1,49 +1,52 @@
-// fichier main.go
 package main
 
 import (
 	"log"
 	"net/http"
+	"os"
 	"url-shortener/internal/api"
 	"url-shortener/internal/db"
 )
 
 func main() {
-	// Connexion à la base
+	// 1. Connexion à la base de données
 	if err := db.Connect(); err != nil {
 		log.Fatal(err)
 	}
 
-	// Création du mux (le routeur)
+	// 2. Création du routeur
 	mux := http.NewServeMux()
 
-	// Setup des routes API sur ce mux
+	// 3. Configuration des routes
 	api.SetupRoutes(mux)
 
-	// Redirection des short codes (toutes les autres routes)
+	// 4. Gestionnaire de redirection global
 	mux.HandleFunc("/", api.RedirectHandler)
 
-	// 1. On enveloppe le routeur avec le Rate Limiter (Sécurité)
+	// 5. Rate Limiter
 	rateLimitedMux := api.RateLimitMiddleware(mux)
 
-	// 2. Middleware CORS (qui enveloppe maintenant le routeur sécurisé)
+	// 6. Configuration du Port dynamique (Standard Hébergeurs/Docker)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Valeur par défaut pour le dev local
+	}
+
+	// 7. Middleware CORS
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Autoriser le front-end
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-		// Répondre aux requêtes OPTIONS (pré-vol)
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
 		}
 
-		// Passer la requête au routeur sécurisé
 		rateLimitedMux.ServeHTTP(w, r)
 	})
 
-	log.Println("Server is running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Printf("Server is running on http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
